685c202cad877fe9c79147be6680faebfca0f55c
devalfe <mdt.webmaster@gmail.com>
2019-02-06 22:21:32 -0500

    cambio oficial de dominio

--- views/templates/programa.php
+++ views/templates/programa.php
@@ -8,14 +8,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-01.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-01.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-02.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-02.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
@@ -27,14 +27,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-03.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-03.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-04.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-04.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
@@ -46,14 +46,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-05.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-05.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-06.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-06.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
@@ -65,14 +65,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-07.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-07.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-08.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-08.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
@@ -84,14 +84,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-09.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-09.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-10.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-10.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
@@ -103,14 +103,14 @@
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
-                               <img class=" img-fluid" src="http://turismo.moquegua.gob.pe/public/images/flayer-11.jpg" alt="card image">
+                               <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-11.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
                     <div class="backside">
                         <div class="card">
                             <div class="card-body text-center mt-4">
-                                <img class=" img-fluid" src="http://moqueguaturismo.gob.pe/public/images/flayer-12.jpg" alt="card image">
+                                <img class=" img-fluid" src="http://turismo.munimoquegua.gob.pe/public/images/flayer-12.jpg" alt="card image">
                             </div>
                         </div>
                     </div>
