c717af34c9f13379640576caa36b93bd6f073fb1
devalfe <mdt.webmaster@gmail.com>
2019-02-27 22:26:01 -0500

    add config prettier

--- views/templates/atractivos.php
+++ views/templates/atractivos.php
@@ -1,6 +1,6 @@
 <div class="internas">
     <section class="bienvenidos">
-        <?php include "views/templates/header.php";?>
+        <?php include "views/templates/header.php"; ?>
         <div class="text-encabezado text-center">
             <div class="container">
                 <h1 class="display-4 wow bounceIn">Atractivos turísticos</h1>
@@ -24,68 +24,85 @@
             <!-- Section Header End -->
         </div>
     </main>
-    <?php //?$link = (new Atractivos) -> selecionarAtractivosController();
-        if(!$_GET){
-            echo '<script> window.location = "index.php?action=atractivos&page=1"</script>';
-            //?header('location:index');
-        }
-        echo '
-        <main class="lugar py-2">
+    <?php
+    if (!$_GET) {
+        echo '<script> window.location = "index.php?action=atractivos&page=1"</script>';
+        //?header('location:index');
+    }
+    echo '<main class="lugar py-2">
             <div class="container">
                 <div class="row">';
-                    $view = (new Atractivos)->serviceAtractivosController($_GET["page"]);
-                    //*var_dump($view);
+    $view = (new Atractivos())->serviceAtractivosController($_GET["page"]);
+    //*var_dump($view);
 
-                    $prev_page_url = $view['prev_page_url'];
-                    $next_page_url = $view['next_page_url'];
-                    $current_page = $view['current_page'];
-                    $last_page = $view['last_page'];
-                    $total = $view['total'];
-                    $per_page = $view['per_page'];
-                    $page = $total/$per_page;
-                    $page = ceil($page);
-                    $lugares = $view['data'];
-                    if($_GET['page']>$page || $_GET['page']<=0){
-                        echo '<script> window.location = "index.php?action=atractivos&page=1"</script>';
-                        //?header("location:atractivos");
-                        exit();
-                    }
-                    foreach($lugares as $row => $item){
-                    echo'<article class="col-lg-4 col-md-6">
+    $prev_page_url = $view['prev_page_url'];
+    $next_page_url = $view['next_page_url'];
+    $current_page = $view['current_page'];
+    $last_page = $view['last_page'];
+    $total = $view['total'];
+    $per_page = $view['per_page'];
+    $page = $total / $per_page;
+    $page = ceil($page);
+    $lugares = $view['data'];
+    if ($_GET['page'] > $page || $_GET['page'] <= 0) {
+        echo '<script> window.location = "index.php?action=atractivos&page=1"</script>';
+        //?header("location:atractivos");
+        exit();
+    }
+    foreach ($lugares as $row => $item) {
+        echo '<article class="col-lg-4 col-md-6">
                             <div class="card card-01">
-                                <img class="card-img-top" src="'.$_ENV['API_IMG'].PUBLIC_.$item['foto_url'].'" alt="">
+                                <img class="card-img-top" src="' .
+            $_ENV['API_IMG'] .
+            PUBLIC_ .
+            $item['foto_url'] .
+            '" alt="">
                                 <div class="card-body">
-                                    <h4 class="card-title">'.$item["titulo"].'</h4>
-                                    <p class="card-text d-flex">'.$item["descripcion"].'</p>
+                                    <h4 class="card-title">' .
+            $item["titulo"] .
+            '</h4>
+                                    <p class="card-text d-flex">' .
+            $item["descripcion"] .
+            '</p>
                                 </div>
-                                <a href="index.php?action=lugares&id='.$item["id"].'" class="btn btn-secondary">Mas Informacion</a>
+                                <a href="index.php?action=lugares&id=' .
+            $item["id"] .
+            '" class="btn btn-secondary">Mas Informacion</a>
                             </div>
                         </article>';
-                    }
-                echo'
-                </div>
+    }
+    echo '</div>
                 <nav aria-label="Page">
                         <ul class="pagination">
                             <li class="page-item';
-                                if ( $prev_page_url == null ){ echo ' disabled'; }
-                            echo'">
+    if ($prev_page_url == null) {
+        echo 'disabled';
+    }
+    echo '">
                                 <a class="page-link" href="index.php?action=atractivos&page=';
-                                echo $_GET["page"]-1; echo'" tabindex="-1">Previous</a>
+    echo $_GET["page"] - 1;
+    echo '" tabindex="-1">Previous</a>
                             </li>';
-                            for ($i=0; $i < $page; $i++) {
-                                echo'<li class="page-item ';
-                                    echo $_GET["page"]==$i+1 ? ' active': ''; echo'">
+    for ($i = 0; $i < $page; $i++) {
+        echo '<li class="page-item ';
+        echo $_GET["page"] == $i + 1 ? ' active' : '';
+        echo '">
                                      <a class="page-link"
-                                        href="index.php?action=atractivos&page='; print ($i+1); echo'">';
-                                            print ($i+1);
-                                echo'</a>
+                                        href="index.php?action=atractivos&page=';
+        print $i + 1;
+        echo '">';
+        print $i + 1;
+        echo '</a>
                                 </li>';
-                            }
-                            echo'<li class="page-item';
-                                if ( $next_page_url == null ){ echo ' disabled'; }
-                                echo '">
+    }
+    echo '<li class="page-item';
+    if ($next_page_url == null) {
+        echo ' disabled';
+    }
+    echo '">
                                 <a class="page-link" href="index.php?action=atractivos&page=';
-                                echo $_GET["page"]+1; echo'">Next</a>
+    echo $_GET["page"] + 1;
+    echo '">Next</a>
                             </li>
                         </ul>
                 </nav>
